1c43d68aacc367cf48917ec41ec76ad765a3a1cb
Dragos Ababei <adragos@funlabs.com>
2020-09-02 11:46:26 +0300

    It's Starting to start along, right there ... here, I mean here

---
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string scene)
+    {
+        BoltNetwork.Instantiate(BoltPrefabs.CustomCube, new Vector3 (Random.Range (-8, 8), 0.0f, Random.Range (-8, 8)), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/SampleSceneMenu.cs
@@ -0,0 +1,46 @@
+﻿using Bolt.Matchmaking;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UdpKit;
+using UnityEngine;
+
+public class SampleSceneMenu : Bolt.GlobalEventListener
+{
+    public void StartServer()
+    {
+        BoltLauncher.StartServer();
+    }
+
+    public void StartClient()
+    {
+        BoltLauncher.StartClient();
+    }
+
+    public override void BoltStartDone()
+    {
+        if (BoltNetwork.IsServer)
+        {
+            //BoltMatchmaking.CreateSession(Guid.NewGuid().ToString(), null, "SampleScene");
+
+            BoltMatchmaking.CreateSession(
+                sessionID: Guid.NewGuid().ToString(),
+                sceneToLoad: "SampleScene"
+            );
+        }
+    }
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        foreach (var session in sessionList)
+        {
+            UdpSession photonSession = session.Value;
+
+            if (photonSession.Source == UdpSessionSource.Photon)
+            {
+                
+                BoltMatchmaking.JoinSession(photonSession);
+            }
+        }
+    }
+}
