52b2527bccd8b0aaa5da4564eac7902848a27f7e
Dragos Ababei <adragos@funlabs.com>
2020-09-02 14:21:42 +0300

    Added movement to the cubes

---
+++ Assets/Scripts/SampleCubeBehaivour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class SampleCubeBehaivour : EntityBehaviour<ICustomCubeState>
+{
+    public override void Attached()
+    {
+        state.SetTransforms(state.CubeTransform, transform);
+    }
+
+    public override void SimulateOwner()
+    {
+        var speed = 4f;
+        var movement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) { movement.z += 1; }
+        if (Input.GetKey(KeyCode.S)) { movement.z -= 1; }
+        if (Input.GetKey(KeyCode.A)) { movement.x -= 1; }
+        if (Input.GetKey(KeyCode.D)) { movement.x += 1; }
+
+        if (movement != Vector3.zero)
+        {
+            transform.position = transform.position + (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
+        }
+    }
+}
